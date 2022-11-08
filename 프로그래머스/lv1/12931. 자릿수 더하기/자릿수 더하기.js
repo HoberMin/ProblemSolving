8fbac95666716bf7770490c1ff16010312ce809c
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-08 14:21:02 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,8 +1,5 @@
 function solution(n)
 {
-    const Narray = (n+"").split("")
-    const answer = Narray.reduce((sum,cur) =>parseInt(sum)+parseInt(cur),0)
+    return [...(n+'')].reduce((sum,cur) =>sum+parseInt(cur),0)
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-
-    return answer;
 }
