7b0c52549089c609f5b2aaa18835cd09e34b76f9
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-08 21:03:41 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,5 +1,5 @@
 function solution(age) {
     const ageArr =(age+"").split("")
-    const answer = ageArr.map((item)=>"abcdefghij"[item])
+    const answer = ageArr.map((item)=>"abcdefghij"[+item])
     return answer.join("")
 }
