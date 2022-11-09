399f4b5a435339f98df4c25a8a92829f82596757
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-09 23:32:20 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,6 +1,5 @@
 function solution(x) {
-    const answer = (x+"").split("")
-    const sum = answer.reduce((sum,cur) => sum+parseInt(cur),0)
+    const sum = (x+"").split("").reduce((sum,cur) => sum+parseInt(cur),0)
     
     return x%sum===0 ? true : false
 }
