b5e2ebc9dc5ea281282fcf34544417360c6816de
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-10 21:45:33 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,6 +1,6 @@
 function solution(seoul) {
-    let a=0
-    seoul.forEach((item,idx)=>{if(item==='Kim'){return a+=idx}})
-    return `김서방은 ${a}에 있다`
-    
+    let a = 0;
+    seoul.forEach((item,idx)=>{if(item==="Kim"){
+        a+=idx}return a} )
+    return `김서방은 ${a}에 있다`;
 }
