8053323f90f17589421ce32b6b14e72bfbe80e23
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-12-21 15:35:01 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    let arr = numbers
+    .sort()
+    .sort((a,b)=>(a.toString()+b.toString())-(b.toString()+a.toString()))
+    .reverse()
+    
+    return arr[0]===0? '0' : arr.join("")
+}
