91c3193af280508153ed5d4c69c11734fc4ee517
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-12-21 13:31:11 +0900

    [level 1] Title: K번째수, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,14 +1,5 @@
 function solution(array, commands) {
-    const commandsLength = commands.length
-    let answer = []
-    let n = 0
-    while(n<commands.length){
-        let i = commands[n][0]
-        let j = commands[n][1]
-        let k = commands[n][2]
-        let arr = array.slice(i-1,j)
-        answer.push(arr.sort((a,b)=>a-b)[k-1])
-        n++
-    }
-    return answer;
+    const answer = commands.map(v=>
+        array.slice(v[0]-1,v[1]).sort((a,b)=>a-b)[v[2]-1])
+    return answer
 }
