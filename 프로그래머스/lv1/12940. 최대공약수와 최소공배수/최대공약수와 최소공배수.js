ff14827845b7a1304750b44483fdb7e1321d200e
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-12-27 13:20:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,16 +1,8 @@
 function solution(n, m) {
-    const divisorN = divisor(n) //[1,3]
-    const divisorM = divisor(m) //[1,2,3,4,6,12]
-    let GCD = Math.max(...divisorN.filter(item=>divisorM.includes(item)))
+    const GCD = gcd(n,m)
     return [GCD,n*m/GCD]
 }
-
-function divisor(number){
-    const arr = []
-    for(let i=0; i<=number; i++){
-        if(number%i===0) arr.push(i)
-    }
-    return arr
+function gcd(n,m){
+    if(m===0)return n
+    return gcd(m,n%m)
 }
-
-//15 27 [1,3,5,15] [1,3,9,27] =>3,5,9
