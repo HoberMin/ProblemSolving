ad0785f7da6b49877b6cda6286ba17c9d6b5c05f
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-01-01 17:49:11 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,6 +1,5 @@
 function solution(brown, yellow) {
     for(let i = 1; i<=yellow; i++){
-        if(yellow%i!==0) continue;
         if((i+2)*(yellow/i+2) === brown+yellow) return [yellow/i+2 , i+2]
     }
     
