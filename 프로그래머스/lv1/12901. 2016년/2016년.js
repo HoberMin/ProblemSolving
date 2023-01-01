bfaf7e016ef54724a02103ccc355457e75d942da
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-01-01 18:52:08 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,5 +1,5 @@
 function solution(a, b) {
-    const sentense = `2016/0${a}/${b}`
+    const sentense = `2016-${a}-${b}`
     const day = new Date(sentense).getDay()
     const answer = ['SUN','MON','TUE','WED','THU','FRI','SAT']
     return answer[day]
