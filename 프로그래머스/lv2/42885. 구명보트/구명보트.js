5a165df7ddc9f4ac16e09a7d555f70fbe0908a75
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-01-03 22:25:06 +0900

    [level 2] Title: 구명보트, Time: 11.77 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,14 +1,14 @@
 function solution(people, limit) {
-    var answer = 0;
+    let answer = 0;
     const sortedPeople = people.sort((a,b)=>a-b);
-    let lInd = 0;
-    let rInd = people.length - 1;
-    while ( lInd <= rInd ) {
-        if ( sortedPeople[lInd] + sortedPeople[rInd] <= limit ) {
-            lInd++;
+    let lower = 0;
+    let higher = people.length - 1;
+    while ( lower <= higher ) {
+        if ( sortedPeople[lower] + sortedPeople[higher] <= limit ) {
+            lower++; // 최저몸무게를 가진 사람은 동행을 했으니 그 다음으로 넘김
         }
-        rInd--;
-        answer += 1;
-    }
+        higher--; // 최대무게를 가진사람과 동행 할 사람이 없으니 보트에 탑승
+        answer += 1; //보트 수 추가
+     }
     return answer;
 }
