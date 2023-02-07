fd52d28c16f55b990a397a6640192ad941bd944b
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-02-07 13:18:38 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,14 +1,3 @@
 def solution(numbers):
-    if len(numbers) == 2:
-        return numbers[0]*numbers[1]
-    positive = [0,0]
-    negative = [0,0]
-    for i in numbers:
-        if(i>0):
-            positive.append(i)
-        else:
-            negative.append(i)
-    positive.sort()
-    negative.sort(reverse=True)    
-    return max(positive[-1]*positive[-2],negative[-1]*negative[-2])
-            
+    numbers.sort()
+    return max(numbers[0]*numbers[1], numbers[-1]*numbers[-2])
