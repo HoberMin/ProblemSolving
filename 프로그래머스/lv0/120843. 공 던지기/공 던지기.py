5f5fbf23a2743c4afee7ab6c0760580db568e274
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-02-13 09:22:02 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -1,8 +1,2 @@
 def solution(numbers, k):
-    idx = 0
-    while(k!=1):
-        idx+=2
-        k-=1
-        if idx > len(numbers)-1:
-            idx -= len(numbers)
-    return numbers[idx]
+    return numbers[2 * (k - 1) % len(numbers)]
