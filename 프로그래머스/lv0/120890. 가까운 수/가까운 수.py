18a2432a041452caaac0f82bab6bf80014800354
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-02-20 15:06:37 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,10 +1,3 @@
-import math
 def solution(array, n):
-    array.sort(reverse=True)
-    gap_min = max(array)
-    answer = min(array)
-    for i in array:
-        if(gap_min>=math.fabs(n-i)):
-            gap_min=math.fabs(n-i)
-            answer = i
-    return answer
+    arr=sorted(array, key=lambda x:(abs(x-n),x),reverse=True)
+    return arr[-1]
