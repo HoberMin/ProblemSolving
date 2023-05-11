008447f7c849dd56d61b7978142097c9026bbbc5
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-05-11 10:39:03 +0900

    [level 2] Title: 구명보트, Time: 8.15 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,14 +1,16 @@
 def solution(people, limit):
-    lower = 0
-    higher = len(people)-1
-    boat =0
+    lower_idx = 0
+    higher_idx = len(people)-1
+    boat_counting = 0
     people.sort()
-    while(lower<=higher):
-        if people[lower]+people[higher] <= limit:
-            lower+=1
-        higher-=1
-        boat +=1
-    return boat
+    while(lower_idx<=higher_idx):
+        if people[lower_idx]+people[higher_idx] <= limit:
+            lower_idx+=1
+        higher_idx-=1
+        boat_counting +=1
+    return boat_counting
 
-# 정렬해야함
-# 제일 가벼운사람 타고 + 무거운사람부터뒤져서 그다음 무거운사람이타야함
+# 최대인원은 두명이기에 무거운사람부터 태운다 1인1보트
+# 단, 1보트에 2인까지 탑승가능하기에 제일무거운사람+제일가벼운사람<=한계 일땐 가벼운사람도 하나 카운팅
+# 무거운사람을 역순으로 내리면서 한명씩 무게를 비교하고 가벼운사람과 무거운사람의 인덱스가 만날 때 모두탑승한것
+# while문이 한번 돌 때 boat증가, 가장 무거운사람은 넘어갔으니 감소
