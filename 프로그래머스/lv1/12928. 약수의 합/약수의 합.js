0e8cd4200c512fe937811f95ba9157b08f422d46
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-07-09 01:04:59 +0900

    [level 1] Title: 약수의 합, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,12 +1,15 @@
 function solution(n) {
-    const arr = new Array(n)
-    let sum = 0;
-    arr.fill(0)
-    arr.map((item,idx)=>{arr[idx]=idx+1})
-    arr.forEach((number)=>{
-        if(n%number==0){
-            sum=sum+number;
+    const divisorArr = divisor(n)
+    return divisorArr.reduce((acc,cur)=>acc+cur,0)
+}
+
+function divisor(n){
+    i=1
+    div = []
+    for(i=1; i<=n; i++){
+        if(n%i===0){
+            div.push(i)
         }
-    })
-    return sum
+    }
+    return div
 }
