2da8b9a20b5253ae12362fd11f4eb98326d1e8e2
HoberMin <102784200+HoberMin@users.noreply.github.com>
2023-07-09 15:51:42 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,3 +1,7 @@
 function solution(numbers) {
-    return 45 - numbers.reduce((sum,cur)=>sum+cur,0)
+    let sum = 0
+    for (i=0; i<=9; i++){
+        if (!numbers.includes(i)) sum += i
+    }
+    return sum
 }
